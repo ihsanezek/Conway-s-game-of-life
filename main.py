ed64c3678e993d3711dc6ca1d83e82b49f65afbc
ihsanezek <ihsane.zkr@gmail.com>
2024-12-18 15:39:35 +0100

    Fonction compute_number_neighbors fonctionnelle

--- main.py
+++ main.py
@@ -17,3 +17,46 @@ frame = numpy.array([[0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0]])
 
 print (frame)
+
+#Fonction compute_number_neighbors 
+
+def compute_number_neighbors (paded_frame, index_line, index_column) :
+    """
+    Cette fonction prend en entrée la matrice avec bordure et
+    renvoie le nombre de cellules voisines vivantes.
+    
+    Returns
+    -------
+    Le nombre de voisin vivant : integer
+
+    """
+    
+    #On extrait la sous matrice autour de la cellule centrale qui est de dimension 3x3
+    neighboors_matrice = paded_frame[index_line - 1 : index_line + 2, index_column - 1 : index_column + 2]
+    
+    #On compte le nombre de voisins vivant sans oublier de retirer la cellule centrale
+    sum_neighboors = numpy.sum(neighboors_matrice) - paded_frame(index_line, index_column)
+
+    return sum_neighboors
+
+
+#Focntion compute_next_frame
+
+def compute_next_frame (frame): 
+    """
+    Cette fonction prend en entrée une frame et calcule la frame suivante
+    à partir des règles du jeu de la vie
+ 
+    Parameters
+    ----------
+    frame : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    paded_frame = numpy.pad(frame, 1, mode="constant")
+    
+    #Etape 1 
